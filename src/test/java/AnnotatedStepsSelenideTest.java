feeb6240fba4a519116aba3ec10dfdc84c4b04a6
AlexeyM <alex_mesch@mail.ru>
2022-07-14 14:46:37 +0300

    added all class

--- src/test/java/AnnotatedStepsSelenideTest.java
+++ src/test/java/AnnotatedStepsSelenideTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class AnnotatedStepsSelenideTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AnnotatedStepsSelenideTest {
+
+    private final String REPOSITORY = "eroshenkoam/allure-example";
+    private final Integer ISSUE_NUMBER = 74;
+
+    @Test
+    @DisplayName("Шаги с аннотацией @Step")
+    public void testWithAnnotatedSteps(){
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchRepository(REPOSITORY);
+        steps.openRepository(REPOSITORY);
+        steps.openTabRepository();
+        steps.shouldSeeIssueWithNumber(ISSUE_NUMBER);
+    }
 }
--- src/test/java/LambdaStepSelenideTest.java
+++ src/test/java/LambdaStepSelenideTest.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class LambdaStepSelenideTest {
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+
+public class LambdaStepSelenideTest {
+
+    private final String REPOSITORY = "eroshenkoam/allure-example";
+    private final Integer ISSUE_NUMBER = 76;
+
+    @Test
+    @DisplayName("Лямбда шаги через step")
+    public void testGitHubIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+        step("Ищем репозиторий по имени " + REPOSITORY, () -> {
+            $(".js-site-search-form").click();
+            $(".input-sm").sendKeys(REPOSITORY);
+            $(".input-sm").submit();
+        });
+        step("В результатах поиска переходим по ссылке репозитория " + REPOSITORY, () -> {
+            $(By.linkText("eroshenkoam/allure-example")).click();
+        });
+        step("Открываем таб Issues", () -> {
+            $(By.partialLinkText("Issues")).click();
+        });
+        step("Проверяем что существует Issue c номером " + ISSUE_NUMBER, () -> {
+            $(withText("#" + ISSUE_NUMBER)).should(Condition.exist);
+        });
+
+
+    }
 }
